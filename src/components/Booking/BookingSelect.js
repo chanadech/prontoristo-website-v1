fed1204d9b05b23db1f1c816c390c394e1944527
Sun-Keera <tokio5189@gmail.com>
2020-12-24 17:36:07 +0700

    Enough is enough, Genshin time

---
+++ src/components/Booking/BookingSelect.js
@@ -0,0 +1,28 @@
+
+export const childrenoptions = [
+    { value: '0', label: '0' },
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' },
+    { value: '5', label: '5' }
+  ]
+  export const guestoptions = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' },
+    { value: '5', label: '5' },
+    { value: '6', label: '6' },
+    { value: '7', label: '7' },
+    { value: '8', label: '8' },
+    { value: '9', label: '9' },
+    { value: '10', label: '10' },
+  ];
+  
+  export const timeoptions = [
+    { value: '10', label: '10:00' },
+    { value: '11', label: '11:00' },
+    { value: '12', label: '12:00' },
+    { value: 'xx', label: 'xx:xx' },
+  ];
--- src/components/Booking/index.js
+++ src/components/Booking/index.js
@@ -1,36 +1,9 @@
-import React, { useState, Component } from "react";
+import React, { useState } from "react";
 import Select from 'react-select'
 import {BookingContainer, BookingButton} from './BookingElements';
 import DatePicker from 'react-date-picker';
+import {guestoptions, childrenoptions, timeoptions} from './BookingSelect'
 
-
-const childrenoptions = [
-  { value: '0', label: '0' },
-  { value: '1', label: '1' },
-  { value: '2', label: '2' },
-  { value: '3', label: '3' },
-  { value: '4', label: '4' },
-  { value: '5', label: '5' }
-]
-const guestoptions = [
-  { value: '1', label: '1' },
-  { value: '2', label: '2' },
-  { value: '3', label: '3' },
-  { value: '4', label: '4' },
-  { value: '5', label: '5' },
-  { value: '6', label: '6' },
-  { value: '7', label: '7' },
-  { value: '8', label: '8' },
-  { value: '9', label: '9' },
-  { value: '10', label: '10' },
-]
-
-const timeoptions = [
-  { value: '10', label: '10:00' },
-  { value: '11', label: '11:00' },
-  { value: '12', label: '12:00' },
-  { value: 'xx', label: 'xx:xx' },
-]
 const Booking = () => {
     const [value, onChange] = useState(new Date());
     return (
@@ -45,7 +18,7 @@ const Booking = () => {
                 onChange={onChange}
                 value={value}/>
             <p>Time</p>
-            <Select name = "TimeSelect" options={timeoptions}clearable={false}/>
+            <Select name = "TimeSelect"   width='10000px' options={timeoptions}clearable={false}/>
             <BookingButton>Confirm Booking</BookingButton>
         </BookingContainer>
   );
