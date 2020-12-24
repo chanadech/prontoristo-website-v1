57e1da140a8d0f15f8754d77bd003c9c347a5472
chanadech <61090003@kmitl.ac.th>
2020-12-24 20:28:06 +0700

    SearchFullEvent

---
+++ src/components/SearchBar/PagesNavigation.js
@@ -0,0 +1,70 @@
+// import React from "react";
+
+// export default (props) => {
+//   const {
+//     loading,
+//     showPrevLink,
+//     showNextLink,
+//     handlePrevClick,
+//     handleNextClick,
+//   } = props;
+//   return (
+//     <div className="nav-link-container">
+//       <a
+//         href="#"
+//         className={`nav-link 
+// 					${showPrevLink ? "show" : "hide"}
+// 					${loading ? "greyed-out" : ""}`}
+//         onClick={handlePrevClick}
+//       >
+//         Prev
+//       </a>
+//       <a
+//         href="#"
+//         className={`nav-link 
+// 					${showNextLink ? "show" : "hide"}
+// 					${loading ? "greyed-out" : ""}
+// 					`}
+//         onClick={handleNextClick}
+//       >
+//         Next
+//       </a>
+//     </div>
+//   );
+// };
+
+import React from 'react'
+
+export default(props) => {
+    const {
+        loading,
+        showPrevLink,
+        showNextLink,
+        handlePrevClick,
+        handleNextClick,
+    } = props;
+    
+    return (
+        <div className = "nav-link-container">
+            <a href ="#"
+            className = {
+                `nav-link
+                ${showPrevLink ? 'show' : 'hide'}
+                ${loading ? 'greyed-out':''}`
+            }
+            onClick = {handlePrevClick}
+            >
+            Prev
+            </a>
+            <a href="#" className={
+                `nav-link
+                ${showNextLink? 'show' : 'hide'}
+                ${loading ? 'greyed-out' : ''}`
+            }
+            onClick = {handleNextClick}
+            >
+                Next
+                </a>
+        </div>
+    )
+}
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,116 +1,179 @@
+import React from "react";
+import "../../components/SearchBar/search.css";
+import axios from "axios";
+import Loader from "../../images/loader.gif";
+import PagesNavigation from "./PagesNavigation";
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-import React from 'react';
-import '../../components/SearchBar/search.css'
-import axios from 'axios';
-import Loader from '../../images/loader.gif';
-class SearchBar extends React.Component{
-    constructor(props){
-        super(props);
-    
     this.state = {
-        query: '',
-        results: {},
-        loading:false,
-        message:''
+      query: "",
+      results: {},
+      loading: false,
+      message: "",
+      totalResults: 0,
+      totalPages: 0,
+      currentPageNo: 0,
     };
-    this.cancel = '';
-}
-fetchSearchResults = (updatedPageNo,query) => {
-  const pageNumber = updatedPageNo ? `&page=4${updatedPageNo}` : '';
-  const searchUrl = `https://pixabay.com/api/?key=12413278-79b713c7e196c7a3defb5330e&q=${query}`
-
-  if(this.cancel){
-    this.cancel.cancel();
+    this.cancel = "";
   }
-  this.cancel = axios.CancelToken.source();
-  axios.get (searchUrl,{
-    cancelToken : this.cancel.token
-  }).then(res => {
-    const resultNotFoundMessage = ! res.data.hits.length 
-    ? 'There are no more search result. Please try a new search'
-    :'';
-    this.setState({
-      results:res.data.hits,
-      messages: resultNotFoundMessage,
-      loading:false
+  getPageCount = (total, denominator) => {
+    const divisible = 0 === total % denominator;
+    const valueToBeAdded = divisible ? 0 : 1;
+    return Math.floor(total / denominator) + valueToBeAdded;
+  };
 
+  fetchSearchResults = (updatedPageNo, query) => {
+    const pageNumber = updatedPageNo ? `&page=${updatedPageNo}` : "";
+    const searchUrl = `https://pixabay.com/api/?key=12413278-79b713c7e196c7a3defb5330e&q=${query}`;
 
-    })
-    
-  } )
-  .catch(error =>{
-    if (axios.isCancel(error) || error) {
-      this.setState({
-        loading:false,
-        message:'Failed to fetch the data, Please Check the network'
-      })
+    if (this.cancel) {
+      this.cancel.cancel();
     }
-  })
-};
+    this.cancel = axios.CancelToken.source();
+    axios
+      .get(searchUrl, {
+        cancelToken: this.cancel.token,
+      })
+      .then((res) => {
+        const total = res.data.total;
+        const totalPagesCount = this.getPageCount(total, 20);
+        const resultNotFoundMessage = !res.data.hits.length
+          ? "There are no more search result. Please try a new search"
+          : "";
+        this.setState({
+          results: res.data.hits,
+          messages: resultNotFoundMessage,
+          totalResults: total,
+          totalPages: totalPagesCount,
+          currentPageNo: updatedPageNo,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        if (axios.isCancel(error) || error) {
+          this.setState({
+            loading: false,
+            message: "Failed to fetch the data, Please Check the network",
+          });
+        }
+      });
+  };
 
-handleOnInputChange = ( event ) => {
-  const query = event.target.value;
-  if ( ! query ) {
-    this.setState( { query, results: {}, message: '', totalPages: 0, totalResults: 0 } );
-  } else {
-    this.setState( { query, loading: true, message: '' }, () => {
-      this.fetchSearchResults( 1, query );
-    } );
-  }
-};
-renderSearchResults = () => {
-  const { results } = this.state;
+  handleOnInputChange = (event) => {
+    const query = event.target.value;
+    if (!query) {
+      this.setState({
+        query,
+        results: {},
+        message: "",
+        totalPages: 0,
+        totalResults: 0,
+      });
+    } else {
+      this.setState({ query, loading: true, message: "" }, () => {
+        this.fetchSearchResults(1, query);
+      });
+    }
+  };
+  handlePageClick = ( event ) => {
+   
+    // event.preventDefault();
 
-  if ( Object.keys( results ).length && results.length ) {
-    return (
-      <div className="results-container">
-        { results.map( result => {
-          return (
-            <a key={ result.id } href={ result.previewURL } className="result-item">
-              <h6 className="image-username">{result.user}</h6>
-              <div className="image-wrapper">
-                <img className="image" src={ result.previewURL } alt={`${result.username} image`}/>
-              </div>
-            </a>
-          )
-        } ) }
+		const updatePageNo = 'prev' === event
+			? this.state.currentPageNo - 1
+			: this.state.currentPageNo + 1;
 
-      </div>
-    )
-  }
-};
+		if( ! this.state.loading  ) {
+			this.setState( { loading: true, message: '' }, () => {
+				this.fetchSearchResults( updatePageNo, this.state.query );
+			} );
+		}
+	};
+  renderSearchResults = () => {
+    const { results } = this.state;
 
+    if (Object.keys(results).length && results.length) {
+      return (
+        <div className="results-container">
+          {results.map((result) => {
+            return (
+              <a
+                key={result.id}
+                href={result.previewURL}
+                className="result-item"
+              >
+                <h6 className="image-username">{result.user}</h6>
+                <div className="image-wrapper">
+                  <img
+                    className="image"
+                    src={result.previewURL}
+                    alt={`${result.username} image`}
+                  />
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      );
+    }
+  };
 
-render(){
-    const {query,loading,message} = this.state;
+  render() {
+    const { query, loading, message, currentPageNo, totalPages } = this.state;
     // const query = this.state.query;
-    console.log(this.state)
+    const showPrevLink = 1 < currentPageNo;
+    const showNextLink = totalPages > currentPageNo;
+    // console.log(this.state);
     return (
-        <div className = "container">
+      <div className="container">
         {/* Heading */}
-        <h2 className="heading">  Restaurant Search</h2>
+        <h2 className="heading"> Restaurant Search</h2>
         {/* Search Input */}
-        <label className="search-label" htmlFor = "search-input">
-            <input
-            type = "text"
-            value = {query}
-            id = "search-input"
-            name = "query"
-            placeholder ="Search your restaurant.."
-            onChange= {this.handleOnInputChange}
-            />
-            <i class="fa fa-search" aria-hidden="true"></i>
-
-        </label> 
+        <label className="search-label" htmlFor="search-input">
+          <input
+            type="text"
+            value={query}
+            id="search-input"
+            name="query"
+            placeholder="Search your restaurant.."
+            onChange={this.handleOnInputChange}
+          />
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </label>
         {/* Error Message */}
         {message && <p className="message">{message}</p>}
 
-
         {/* loader */}
-        <img src = {Loader} className ={`search-loading ${loading ? 'show' : 'hide'}`} alt = "loader"/>
+        <img
+          src={Loader}
+          className={`search-loading ${loading ? "show" : "hide"}`}
+          alt="loader"
+        />
+
+        {/*Navigation*/}
+        <PagesNavigation
+          loading={loading}
+          showPrevLink={showPrevLink}
+          showNextLink={showNextLink}
+          handlePrevClick={() => this.handlePageClick('prev',this.state.event)}
+          handleNextClick={() =>this.handlePageClick('next',this.state.event)}
+        />
+
         {/* Result    */}
         {this.renderSearchResults()}
-        </div>
-    )}}
-    export default SearchBar;
+
+        {/*Navigation*/}
+        <PagesNavigation
+          loading={loading}
+          showPrevLink={showPrevLink}
+          showNextLink={showNextLink}
+          handlePrevClick={() =>this.handlePageClick('prev',this.state.event)}
+          handleNextClick={() =>this.handlePageClick('next',this.state.event)}
+        />
+      </div>
+    );
+  }
+}
+export default SearchBar;
